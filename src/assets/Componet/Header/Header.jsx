6d3bd34afb3f988d511e8ad679b6ff1131a5e8d0
asifaktar <asifaktar7767@gmail.com>
2025-01-09 22:40:25 +0530

    added nevigetion

--- src/assets/Componet/Header/Header.jsx
+++ src/assets/Componet/Header/Header.jsx
@@ -1,17 +1,21 @@
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
     return (
         <div>
             <h1>NavBer</h1>
             <ul>
-                <Link to="/">Home</Link>
-                <Link to="/Contact">Contact</Link>
-                <Link to ="/posts">Posts</Link>
-                <Link to="/Service">Service</Link>
-                <Link to="/User">User</Link>
-                <Link to="/About">AboutUs</Link>
+                {/* <Link to="/">Home</Link> */}
+                <NavLink to ="/">Home</NavLink>
+                {/* <Link to="/Contact">Contact</Link> */}
+                <NavLink to ="/Contact">Contact</NavLink>
+                {/* <Link to ="/posts">Posts</Link> */} 
+                <NavLink to ="/posts">Posts</NavLink>
+                {/* <Link to="/User">User</Link> */}
+                <NavLink to ="/User">User</NavLink>
+                {/* <Link to="/About">AboutUs</Link> */}
+                <NavLink to ="/About">About</NavLink>
             </ul>
         </div>
     );
